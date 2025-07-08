d208d6cd8eede9306bb3e525a95626bddb0fbc61
cattiaux <martin.cattiaux@gmail.com>
2025-07-08 15:41:45 -0300

    work on ingredientPrice admin

--- pastry_app/admin.py
+++ pastry_app/admin.py
@@ -47,15 +47,12 @@ class IngredientPriceAdmin(admin.ModelAdmin):
     class Media:
         css = {'all': ('pastry_app/admin/required_fields.css',)}
 
-class IngredientPriceInline(admin.StackedInline):
-    model = IngredientPrice
-    extra = 1  # number of extra forms to display
-
 @admin.register(IngredientPriceHistory)
 class IngredientPriceHistoryAdmin(admin.ModelAdmin):
     list_display = ("id", "ingredient", "brand_name", "store", "quantity", "unit", "price", "is_promo", "promotion_end_date", "date")
     list_filter = ("brand_name", "is_promo", StoreCityListFilter, StoreNameListFilter)
     search_fields = ("ingredient__ingredient_name", "brand_name", "store__store_name")
+    date_hierarchy = 'date'
     
     def has_add_permission(self, request):
         """ Empêche l'ajout manuel d'entrées historiques sauf pour les super-utilisateurs. """
@@ -68,88 +65,6 @@ class IngredientPriceHistoryAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         """ Empêche la suppression d'entrées historiques sauf pour les super-utilisateurs. """
         return request.user.is_superuser
-
-class IngredientAdmin(admin.ModelAdmin):
-    inlines = [IngredientPriceInline]
-    list_display = ('ingredient_name', 'id')
-
-    # To see only the categories for the selected ingredient in the django admin site
-    # def formfield_for_manytomany(self, db_field, request, **kwargs):
-    #     if db_field.name == "categories":
-    #         if 'change' in request.path:
-    #             ingredient_id = request.path.split('/')[-3]
-    #             ingredient = Ingredient.objects.get(id=ingredient_id)
-    #             kwargs["queryset"] = Category.objects.filter(ingredients=ingredient)
-    #     return super().formfield_for_manytomany(db_field, request, **kwargs)
-
-class RecipeIngredientInline(admin.TabularInline):
-    model = RecipeIngredient
-    extra = 1
-
-class RecipeStepInline(admin.TabularInline):
-    model = RecipeStep
-    extra = 1
-
-class SubRecipeInline(admin.TabularInline):
-    model = SubRecipe
-    fk_name = 'recipe'
-    extra = 1
-
-class RecipeAdmin(admin.ModelAdmin):
-    inlines = [RecipeIngredientInline, RecipeStepInline, SubRecipeInline]
-    list_display = ('recipe_name', 'id', 'chef_name')
-    exclude = ('content_type', 'object_id',)
-
-class RecipeIngredientAdmin(admin.ModelAdmin):
-    list_display = ('recipe_name', 'ingredient_name', 'id')
-
-    def recipe_name(self, obj):
-        return obj.recipe.recipe_name
-    recipe_name.short_description = 'Recipe Name'  # Sets column name in admin site
-
-    def ingredient_name(self, obj):
-        return obj.ingredient.ingredient_name
-    ingredient_name.short_description = 'Ingredient Name'  # Sets column name in admin site
-
-class RecipeStepAdmin(admin.ModelAdmin):
-    list_display = ('recipe_name', 'id')
-
-    def recipe_name(self, obj):
-        return obj.recipe.recipe_name
-    recipe_name.short_description = 'Recipe Name'
-
-class SubRecipeAdmin(admin.ModelAdmin):
-    list_display = ('recipe_name', 'subrecipe_name', 'id')
-
-    def recipe_name(self, obj):
-        return obj.recipe.recipe_name
-    recipe_name.short_description = 'Recipe Name'
-
-    def subrecipe_name(self, obj):
-        return obj.sub_recipe.recipe_name
-    subrecipe_name.short_description = 'Subrecipe Name'
-
-class PanAdmin(admin.ModelAdmin):
-    list_display = ('pan_name', 'id', 'pan_brand', 'pan_type', 'units_in_mold', 'visibility', 'is_default')
-
-    fieldsets = (
-        ('Caractéristiques du moule', {
-            'fields': (
-                'pan_name', 'pan_type', 'pan_brand', 
-                'diameter', 'height', 
-                'length', 'width', 'rect_height', 
-                'volume_raw', 'is_total_volume', 'unit'
-            )
-        }),
-        ('Gestion / Droits', {
-            'fields': ('visibility', 'is_default', 'user', 'guest_id'),
-            'description': "Champs relatifs à la visibilité, aux droits, ou à la gestion multi-utilisateur."
-        }),
-    )
-
-    class Media:
-        js = ('pastry_app/admin/pan_admin.js',)
-        css = {'all': ('pastry_app/admin/required_fields.css',)}
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -227,6 +142,94 @@ class StoreAdmin(admin.ModelAdmin):
 
     class Media:
         css = {'all': ('pastry_app/admin/required_fields.css',)}
+
+@admin.register(Pan)
+class PanAdmin(admin.ModelAdmin):
+    list_display = ('pan_name', 'id', 'pan_brand', 'pan_type', 'units_in_mold', 'visibility', 'is_default')
+
+    fieldsets = (
+        ('Caractéristiques du moule', {
+            'fields': (
+                'pan_name', 'pan_type', 'pan_brand', 
+                'diameter', 'height', 
+                'length', 'width', 'rect_height', 
+                'volume_raw', 'is_total_volume', 'unit'
+            )
+        }),
+        ('Gestion / Droits', {
+            'fields': ('visibility', 'is_default', 'user', 'guest_id'),
+            'description': "Champs relatifs à la visibilité, aux droits, ou à la gestion multi-utilisateur."
+        }),
+    )
+
+    class Media:
+        js = ('pastry_app/admin/pan_admin.js',)
+        css = {'all': ('pastry_app/admin/required_fields.css',)}
+
+class IngredientPriceInline(admin.StackedInline):
+    model = IngredientPrice
+    extra = 1  # number of extra forms to display
+
+class IngredientAdmin(admin.ModelAdmin):
+    inlines = [IngredientPriceInline]
+    list_display = ('ingredient_name', 'id')
+
+    # To see only the categories for the selected ingredient in the django admin site
+    # def formfield_for_manytomany(self, db_field, request, **kwargs):
+    #     if db_field.name == "categories":
+    #         if 'change' in request.path:
+    #             ingredient_id = request.path.split('/')[-3]
+    #             ingredient = Ingredient.objects.get(id=ingredient_id)
+    #             kwargs["queryset"] = Category.objects.filter(ingredients=ingredient)
+    #     return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+class RecipeStepInline(admin.TabularInline):
+    model = RecipeStep
+    extra = 1
+
+class SubRecipeInline(admin.TabularInline):
+    model = SubRecipe
+    fk_name = 'recipe'
+    extra = 1
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline, RecipeStepInline, SubRecipeInline]
+    list_display = ('recipe_name', 'id', 'chef_name')
+    exclude = ('content_type', 'object_id',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'ingredient_name', 'id')
+
+    def recipe_name(self, obj):
+        return obj.recipe.recipe_name
+    recipe_name.short_description = 'Recipe Name'  # Sets column name in admin site
+
+    def ingredient_name(self, obj):
+        return obj.ingredient.ingredient_name
+    ingredient_name.short_description = 'Ingredient Name'  # Sets column name in admin site
+
+class RecipeStepAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'id')
+
+    def recipe_name(self, obj):
+        return obj.recipe.recipe_name
+    recipe_name.short_description = 'Recipe Name'
+
+class SubRecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe_name', 'subrecipe_name', 'id')
+
+    def recipe_name(self, obj):
+        return obj.recipe.recipe_name
+    recipe_name.short_description = 'Recipe Name'
+
+    def subrecipe_name(self, obj):
+        return obj.sub_recipe.recipe_name
+    subrecipe_name.short_description = 'Subrecipe Name'
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
